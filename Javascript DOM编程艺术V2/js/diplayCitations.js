7842b6306bd9bbfd7b9e6771c3618edd2eda31a0
kpengWang <417940197@qq.com>
2018-07-15 21:56:57 +0800

    新增：《新增：编写高质量JavaScript代码的68个有效方法》

--- Javascript DOM编程艺术V2/js/diplayCitations.js
+++ Javascript DOM编程艺术V2/js/diplayCitations.js
@@ -2,26 +2,26 @@
  * Created by Kaiser on 16/6/7.
  */
 function displayCitations() {
-//      获取所有引用
-    var quotes = document.getElementsByTagName('blockquote');
-//      遍历引用
-    for (var i = 0; i < quotes.length; i++) {
-        var url = quotes[i].getAttribute('cite');
-        if (!url) continue;
-        var quoteChildren = quotes[i].getElementsByTagName('*');
-        if (quoteChildren.length < 1) continue;
-        var elem = quoteChildren[quoteChildren.length - 1];
-        var links = document.createElement('a');
-        var links_text = document.createTextNode('source');
-        links.appendChild(links_text);
+  //      获取所有引用
+  var quotes = document.getElementsByTagName('blockquote')
+  //      遍历引用
+  for (var i = 0; i < quotes.length; i++) {
+    var url = quotes[i].getAttribute('cite')
+    if (!url) continue
+    var quoteChildren = quotes[i].getElementsByTagName('*')
+    if (quoteChildren.length < 1) continue
+    var elem = quoteChildren[quoteChildren.length - 1]
+    var links = document.createElement('a')
+    var links_text = document.createTextNode('source')
+    links.appendChild(links_text)
 
-        links.setAttribute('href', url);
-        links.setAttribute('title', url);
-        links.setAttribute('target', '_blank');
+    links.setAttribute('href', url)
+    links.setAttribute('title', url)
+    links.setAttribute('target', '_blank')
 
-        var superscript = document.createElement('sup');
-        superscript.appendChild(links);
-        elem.appendChild(superscript);
-    }
+    var superscript = document.createElement('sup')
+    superscript.appendChild(links)
+    elem.appendChild(superscript)
+  }
 }
-addLoadEvent(displayCitations);
+addLoadEvent(displayCitations)
